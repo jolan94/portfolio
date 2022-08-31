9cac1f6f08ba428eba66af156755faa227b19d8e
jolan94 <jolan94@gmail.com>
2022-08-31 12:31:43 +0530

    appbar completion

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -1,20 +1,30 @@
-import React from "react";
+import { React, useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Appbar(props) {
+  const navRef = useRef();
+
+  const onToggleClick = (e) => {
+    navRef.current.classList.toggle("show");
+  };
+
   return (
     <div className="appbar">
-      <div className="titleicon">
-        <h1>Joe</h1>
-      </div>
-      <div className="menuitems">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Projects</li>
-          <li>Contact</li>
-        </ul>
-      </div>
+      <header>
+        <h3>Joe</h3>
+        <nav ref={navRef}>
+          <a href="/#">Home</a>
+          <a href="/#">Skills</a>
+          <a href="/#">Work</a>
+          <a href="/#">Contact</a>
+          <button className="nav-btn nav-close-btn" onClick={onToggleClick}>
+            <FaTimes />
+          </button>
+        </nav>
+        <button className="nav-btn" onClick={onToggleClick}>
+          <FaBars />
+        </button>
+      </header>
     </div>
   );
 }
